42ee135e16779a4ab021491af95a6d8c383abff4
fbnjeje <fabiangomes2015@gmail.com>
2023-12-30 00:38:36 -0500

    advances

--- felichostiguarG/src/App.tsx
+++ felichostiguarG/src/App.tsx
@@ -1,12 +1,26 @@
-import './App.css'
+import Footer from "./app/shared/components/footer/Footer"
+import Header from "./app/shared/components/header/Header"
+
+
+import Home from './app/pages/home/Home'
+
+import { Routes, Route } from "react-router-dom"
+import ProductDetail from "./app/pages/product-detail/ProductDetail"
+
 
 function App() {
-
-  return (
+  return(    
     <>
-      hola gente
+      <Header />
+      <Routes>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/product-detail/:id" element={ <ProductDetail /> } />
+      </Routes>
+
+      <Footer/>
     </>
   )
 }
 
 export default App
+
---
+++ felichostiguarG/src/app/shared/components/header/Header.tsx
@@ -0,0 +1,6 @@
+import './index.css'
+export default function Header() {
+    return (
+        <div>Header</div>
+    )
+}

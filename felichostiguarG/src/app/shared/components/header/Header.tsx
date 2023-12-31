0d84a5a7e041405fccac6b83eeadff3e6500292a
Pandax <fabiangomes2015Q@gmail.com>
2023-12-31 11:45:47 -0500

    header firsts steps

--- felichostiguarG/src/app/shared/components/header/Header.tsx
+++ felichostiguarG/src/app/shared/components/header/Header.tsx
@@ -1,6 +1,25 @@
 import './index.css'
+
 export default function Header() {
+    
     return (
-        <div>Header</div>
+        <div className='header'>
+            <section className='content-left'>
+            <img className='logo' src="https://www.tailorbrands.com/wp-content/uploads/2020/07/twitter-logo.jpg" alt="" />              
+            </section>
+            <section className='content-right'>
+                <ul className='items'>
+                    <li className='item'>Home</li>
+                    <li className='item'>Shows</li>
+                    <li className='item'>Movies</li>
+                    <li className='item'>TV</li>
+                    <li className='item'>|</li>
+                    <li className='item'>Login</li>
+                    <li className='item'>
+                        search
+                    </li>
+                </ul>
+            </section>
+        </div>
     )
 }
